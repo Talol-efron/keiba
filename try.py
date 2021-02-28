35f23669e764c01358f08d744de6c40a5b895695
Taro Asahina <e205713@ie.u-ryukyu.ac.jp>
2021-02-28 15:17:59 +0900

    try donegit add . Probably final commit

--- try.py
+++ try.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import time
 
+######小規模テスト用######
+
 #始め4桁:20xx年 今回は2020
 #中2桁: 開催都市　01;札幌 02;函館 03;福島 04;新潟 05;東京 06;中山 07;中京 08;京都 09;阪神 10;小倉
 #中2桁: x回(1~5)
 #中2桁: x日目(1~9)
 #最後2桁: xR(ラウンド(1~12))
+
 
 def scrape_race_results(race_id_list):
     race_result = {}
@@ -25,15 +28,11 @@ def scrape_race_results(race_id_list):
 
 ##race_id_listをネットからとってきて全部scrape_race_results で表す
 
-#race_list = []
-#str_citeis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-#times = 5
-
 race_list = []
-citeis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-times = ["1", "2", "3", "4", "5"]
-days = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-rounds = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+citeis = ["1", "2"]
+times = ["1", "2"]
+days = ["1", "2"]
+rounds = ["1", "2", "3"]
 
 
 def search_race_id():
@@ -62,6 +61,7 @@ def search_race_id():
     #race_id_list = list(map(int, race_list))
 
     return race_list
+
 
 test = scrape_race_results(search_race_id())
 print(test)
